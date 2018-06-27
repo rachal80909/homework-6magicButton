4cbd08c317b2548185d3c8bbbcaf13307365bd64
Rachal Metz <[rachal80909@yahoo.com]>
2018-06-27 14:25:19 -0600

    changes

--- js/app.js
+++ js/app.js
@@ -20,49 +20,57 @@ $("document").ready(function() {
             //line 9 grabbing the div to say where we want button
 
             //sets up click event
-            <
-            script type = "text/javascript" > < /script>
+            // <script type = "text/javascript" > </script>
 
             $(document).on("click", "button", function() {
-                let searchParameter = $(this).attr('data-type');
-                //holds the serer api url
-                var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + searchParameter;
-                //https://developers.giphy.com/docs/
+                        let searchParameter = $(this).attr('data-type');
+                        //holds the serer api url
+                        //https://developers.giphy.com/docs/
 
-                //creates an ajax request using the get method
-                $.ajax({
-                        url: queryURL,
-                        method: "GET"
-                    }) <
-                    script type = "text/javascript" >
-                    $(".gif").on("click", function() {
-                        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-                        var state = $(this).attr("data-state");
-                        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+                        //creates an ajax request using the get method
 
-                        // Then, set the image's data-state to animate
-                        // Else set src to the data-still value
-                        if (state === "still") {
-                            $(this).attr("src", $(this).attr("data-animate"));
-                            $(this).attr("data-state", "animate");
-                        } else {
-                            $(this).attr("src", $(this).attr("data-still"));
-                            $(this).attr("data-state", "still");
-                        }
-                    }); < /script>
-                //callback function receiver
-                // .done(function(response) {
-                //     console.log(response)
-                //         //create variable of image url, it's a strings
-                //     var imageUrl = response.data.image_original_url;
+                    }
 
-                //     //use jquery to create HTML element o ftype image, img
-                //     var catImage = $("<img>");
+                    <
+                    script type = "text/javascript" > < /script>
+                    $('button').on('click', function() {
+                            var x = $(this.data("search");
+                                var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + searchParameter; $.ajax({
+                                    url: queryURL,
+                                    method: "GET"
+                                }) <
+                                script type = "text/javascript" > < /script>
+                                $(".gif").on("click", function() {
+                                    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+                                    var state = $(this).attr("data-state");
+                                    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
 
-                //     //set the source and alt of our image, to prev define url and cat image respectively
-                //     catImage.attr("src", imageUrl);
-                //     catImage.attr("alt", "cat image");
+                                    // Then, set the image's data-state to animate
+                                    // Else set src to the data-still value
+                                    if (state === "still") {
+                                        $(this).attr("src", $(this).attr("data-animate"));
+                                        $(this).attr("data-state", "animate");
+                                    } else {
+                                        $(this).attr("src", $(this).attr("data-still"));
+                                        $(this).attr("data-state", "still");
+                                    )
+                                })
+                            });
 
-                //     //prepend the image to our images div (that's the div with an id of images)
-                //     $("#images").prepend(catImage);
-            });
+
+
+                        //callback function receiver
+                        // .done(function(response) {
+                        //     console.log(response)
+                        //         //create variable of image url, it's a strings
+                        //     var imageUrl = response.data.image_original_url;
+
+                        //     //use jquery to create HTML element o ftype image, img
+                        //     var catImage = $("<img>");
+
+                        //     //set the source and alt of our image, to prev define url and cat image respectively
+                        //     catImage.attr("src", imageUrl);
+                        //     catImage.attr("alt", "cat image");
+
+                        //     //prepend the image to our images div (that's the div with an id of images)
+                        //     $("#images").prepend(catImage);
